0b044a8734a1f8d9ad68ddc7b5c0935b602d1285
Dale Weiler <killfieldengine@gmail.com>
2013-12-21 05:55:30 -0500

    No calc

--- modules/dance.c
+++ modules/dance.c
@@ -1,12 +1,13 @@
 #include <module.h>
+#include <string.h>
 
 MODULE_DEFAULT(dance);
 
 void module_enter(irc_t *irc, const char *channel, const char *user, const char *message) {
-    if (!message)
+    if (!message || !strlen(message))
         irc_action(irc, channel, "dances like a jolly idiot");
     else
-        // TODO: user list checl
+        // TODO: user list check
         irc_action(irc, channel, "gives %s a lap dance - oooh-lah-lah", message);
 }
 
