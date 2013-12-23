135d859e76c2f32005ea0cb46ccdaff1ac613dcf
Dale Weiler <killfieldengine@gmail.com>
2013-12-22 19:32:49 -0500

    s/q66/%s/

--- modules/quit.c
+++ modules/quit.c
@@ -6,7 +6,7 @@ MODULE_DEFAULT(quit);
 
 void module_enter(irc_t *irc, const char *channel, const char *user, const char *message) {
     if (strcmp(user, "graphitemaster"))
-        irc_write(irc, channel, "q66: sorry, you're not allowed to quit");
+        irc_write(irc, channel, "%s: sorry, you're not allowed to quit", user);
     else
         raise(SIGINT);
 }
