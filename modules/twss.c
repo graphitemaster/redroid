78ddf74ce039e7ea76e412d960bda6d8c18acdce
Dale Weiler <killfieldengine@gmail.com>
2014-01-19 23:27:24 -0500

    Fix it

--- modules/twss.c
+++ modules/twss.c
@@ -11,6 +11,7 @@ void module_enter(irc_t *irc, const char *channel, const char *user, const char 
     if (regexpr_execute(reg, message, 0, NULL)) {
         database_request(irc, "TWSS");
         irc_write(irc, channel, "%s: They've said it a total of %d times",
+            user,
             database_request_count(irc, "TWSS"));
     }
 }
