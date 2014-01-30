7b38bc9c86d7a99a8eb5071b5917ca5eb2afc68a
Dale Weiler <killfieldengine@gmail.com>
2014-01-30 16:31:23 -0500

    Add the system module.

---
+++ modules/system.c
@@ -0,0 +1,28 @@
+#include <module.h>
+#include <string.h>
+
+MODULE_DEFAULT(system);
+
+void module_enter(irc_t *irc, const char *channel, const char *user, const char *message) {
+    if (!message)
+        goto help;
+
+    if (!strcmp(message, "-shutdown")) {
+        /*
+         * TODO: check if user is an administrator and can perform the
+         *       shutdown.
+         */
+        return redroid_shutdown(irc, channel, user);
+    }
+
+    if (!strcmp(message, "-restart")) {
+        /*
+         * TODO: check if user is an administrator and can perform the
+         *       restart.
+         */
+        return redroid_restart(irc, channel, user);
+    }
+
+help:
+    irc_write(irc, channel, "%s: system <-shutdown|-restart>", user);
+}
