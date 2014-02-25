aa21b75ce87bcfcd033934a6d4fe46268fbb8635
Dale Weiler <killfieldengine@gmail.com>
2014-02-25 16:24:40 -0500

    Minor patch level for Redroid versioning scheme.

--- timestamp.c
+++ timestamp.c
@@ -2,6 +2,7 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 8
+#define VERSION_PATCH 0
 
 const char *build_date() {
     static const char *compile_date = __DATE__;
@@ -16,8 +17,11 @@ const char *build_time() {
 const char *build_version() {
     static char table[128] = {0};
     if (!*table) {
-        snprintf(table, sizeof(table), "Redroid v%d.%d (built on %s at %s)",
-            VERSION_MAJOR, VERSION_MINOR, build_date(), build_time());
+        snprintf(table, sizeof(table),
+            "Redroid v%d.%d.%d (built on %s at %s)",
+            VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH,
+            build_date(), build_time()
+        );
     }
     return table;
 }
