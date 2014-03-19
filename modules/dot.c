bdcc0edf92a408122ea1a569789ddbcad1d0d5e2
TheLastProject <rubenvanos@gmx.com>
2014-03-19 17:32:46 +0100

    Added dot module

---
+++ modules/dot.c
@@ -0,0 +1,14 @@
+#include <module.h>
+
+MODULE_ALWAYS(dot);
+
+static const char dotchars[] = ".!?\xa1\xbf"; // The last two are "upside down" versions of the exclamation point and question mark
+
+static bool pass(const char *string) { for (const char *a = string; *a; a++) if (!strchr(dotchars, *a)) return false; return true; }
+
+void module_enter(irc_t *irc, const char *channel, const char *user, const char *message) {
+    if (message && pass(message)) {
+        string_t *reply = string_format("%s%c", message, dotchars[urand() % (sizeof(dotchars) - 1)]);
+        irc_write(irc, channel, string_contents(reply));
+    }
+}
