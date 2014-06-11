9c7d9efde4c91cb44077a380f465a0abdb0deec2
Dale Weiler <weilercdale@gmail.com>
2014-06-11 12:04:15 -0400

    Cleaner handling.

--- command.c
+++ command.c
@@ -128,10 +128,8 @@ bool cmd_channel_pop(cmd_channel_t *channel, cmd_entry_t **output) {
 
         while (channel->head == channel->tail) {
             pthread_cond_wait(&channel->waiter, &channel->mutex);
-            if (channel->wrend && channel->head == channel->tail) {
-                pthread_mutex_unlock(&channel->mutex);
-                return false;
-            }
+            if (channel->wrend && channel->head == channel->tail)
+                break;
         }
 
         channel->rdintent = NULL;
